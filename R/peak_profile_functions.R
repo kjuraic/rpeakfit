084ea74c785d704f032ba29165ff8c4bf23b1986
kjuraic <kjuraic@irb.hr>
2017-11-27 11:26:48 +0100

    verzija za Danijela

--- R/peak_profile_functions.R
+++ R/peak_profile_functions.R
@@ -1,6 +1,7 @@
 #' Gaussian amplitud
 #' @description Gaussian amplitude function
 #' @author K. Juraic
+#' @references PeakFit v4.11
 #' @param x x value
 #' @param y0 backgroubd
 #' @param A amplitude
@@ -17,6 +18,7 @@ gaussianAmpl <- function(x, y0, A, xc, w){
 #' Gaussian area
 #' @description Gaussian area function
 #' @author K. Juraic
+#' @references PeakFit v4.11
 #' @param x x value
 #' @param y0 backgroubd
 #' @param A amplitude
@@ -31,3 +33,37 @@ gaussianArea <- function(x, y0, A, xc, w){
 }
 
 
+#' Lorentzian amplitude
+#' @description Lorentzian amplitude function
+#' @author K. Juraic
+#' @references PeakFit v4.11
+#' @param x x value
+#' @param y0 constant background
+#' @param A amplitude
+#' @param xc peak center
+#' @param w peak width
+#' @return y y value
+#' @examples
+#'    \dontrun{lorentzianAmpl(x = 0.1, y0 = 0, A = 1, xc = 0, w = 1)}
+lorentzianAmpl <- function(x = 0.1, y0 = 0, A = 1, xc = 0, w = 1){
+  y = A / (1 + ((x - xc) / w)^2)
+  return(y)
+}
+
+
+#' Lorentzian area
+#' @description Lorentzian area function
+#' @author K. Juraic
+#' @references PeakFit v4.11
+#' @param x x value
+#' @param y0 constant background
+#' @param A amplitude
+#' @param xc peak center
+#' @param w peak width
+#' @return y y value
+#' @examples
+#'    \dontrun{lorentzianArea(x = 0.1, y0 = 0, A = 1, xc = 0, w = 1)}
+lorentzianArea <- function(x = 0.1, y0 = 0, A = 1, xc = 0, w = 1){
+  y = A / ((1 + (pi * w * (x - xc) / w)^2))
+  return(y)
+}
